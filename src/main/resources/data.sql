e2ea8cdd56cc8ee1698a795f688e73515964e63d
erisson.batista@ask.com <erissonbreno@hotmail.com>
2023-12-23 22:03:21 -0300

    [SOOS-15] Atualiza carga inicial

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,11 @@
-insert into product (name,price,id) values ('Laptop',8900.00,'b16f2fe1-58d1-4c7e-b071-0c19b1c666ec');
-insert into product (name,price,id) values ('iPhone',3500.00,'09511737-f447-4d92-9e0f-a50459200f66');
-insert into product (name,price,id) values ('MacBook',18900.00,'c711eade-74de-4bec-8714-425db6be6082');
-insert into product (name,price,id) values ('Monitor 19',1900.00,'9298d6c6-0762-4d08-bede-8ee35a3be8e5');
-insert into product (name,price,id) values ('TV',9900.00,'9caa1c96-4fd9-47fa-8592-137b683ebde5');
+insert into product (id,name,price,description,is_featured,is_on_offer,category)
+values ('09511737-f447-4d92-9e0f-a50459200f66','iPhone',3500.00,'Laptop Dell',true,false,'ELECTRONIC');
+
+insert into product (id,name,price,description,is_featured,is_on_offer,category)
+values ('c711eade-74de-4bec-8714-425db6be6082','MacBook',18900.00,'Macbook M2',true,true,'ELECTRONIC');
+
+insert into product (id,name,price,description,is_featured,is_on_offer,category)
+values ('9298d6c6-0762-4d08-bede-8ee35a3be8e5','Pão',2.30,'Pão de Sal',false,false,'FOOD');
+
+insert into product (id,name,price,description,is_featured,is_on_offer,category)
+values ('9caa1c96-4fd9-47fa-8592-137b683ebde5','Coquinha',8.99,'Coquinha Zero Gelada',false,true,'BEVERAGE');
