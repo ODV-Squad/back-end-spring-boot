642cba0126e8d009ccdb53be4a2788df782cc938
erisson.batista@ask.com <erissonbreno@hotmail.com>
2023-12-26 10:35:35 -0300

    [SOOS-55] WIP - Home

---
+++ src/main/java/com/br/ebatista/springboot/controller/HomeController.java
@@ -0,0 +1,9 @@
+package com.br.ebatista.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/home")
+public class HomeController {
+}
